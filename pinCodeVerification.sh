7b28d87876e9112d231baa69e7158878fa1a8361
Preranashinde <preranapshinde3009@gmail.com>
2020-03-27 20:10:43 -0300

    To validate 6 digit PIN code

---
+++ pinCodeVerification.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PINCODE="^[1-9][0-9]{5}$"
+
+read -p "Enter six digit pincode ::" pincode
+
+if [[ $pincode =~ $PINCODE ]]; then
+	echo Valid Pincode
+else
+	echo Invalid pincode
+fi
